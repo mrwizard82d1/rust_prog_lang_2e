fbe3e091c3261e5e6971971f6d3b08fd93686889
Larry Jones <mrwizard82d1@gmail.com>
2024-10-26 14:17:58 -0500

    Initial implementation of `guessing_game`

--- code/guessing_game/src/main.rs
+++ code/guessing_game/src/main.rs
@@ -1,3 +1,20 @@
+// Import the standard I/O library
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Print instructions to the user
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    // Initialize the user's guess
+    let mut guess = String::new();
+
+    // Read the guess from the user
+    io::stdin()
+        .read_line(&mut guess)
+        // Expect to read a value; otherwise, an error occurred
+        .expect("Failed to read line");
+
+    // Print out the user's guess
+    println!("You guessed: {}", guess);
 }
