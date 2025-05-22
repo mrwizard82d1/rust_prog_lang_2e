7d502eebbffa2282bb45a943d9a5468efdc5426c
Larry Jones <mrwizard82d1@gmail.com>
2025-05-22 17:37:58 -0500

    Shadow a variable with a different type variable

--- ch03/shadowing/src/main.rs
+++ ch03/shadowing/src/main.rs
@@ -9,4 +9,14 @@ fn main() {
         println!("Shadow x again. It's value in the **inner** scope is {x}.");
     }
     println!("The value of x in the **outer** scope is visible {x}!");
+
+    println!("We can also change the **type** when we shadow a variable.");
+
+    println!();
+
+    let spaces = "    ";
+    println!("Tough to see `spaces` without angle brackets: <{spaces}>.");
+
+    let spaces = spaces.len();
+    println!("But `spaces.len()` returns a u32 (I think): {spaces}.");
 }
