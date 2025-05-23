3d05bf970ab01aa6801d199385c512024c547a06
Larry Jones <mrwizard82d1@gmail.com>
2025-05-23 16:34:20 -0500

    Boolean values

---
+++ ch03/boolean/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let t = true;
+    println!("A true value: {t}");
+
+    // Using explicit type annotation
+    let f: bool = false;
+    println!("A (explicitly typed) false value: {f}");
+}
