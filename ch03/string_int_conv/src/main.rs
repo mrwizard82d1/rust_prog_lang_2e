d6ad1c7d8002080a58ca114edbf8001c0027679b
Larry Jones <mrwizard82d1@gmail.com>
2025-05-23 13:55:52 -0500

    Correct error by adding type information

--- ch03/string_int_conv/src/main.rs
+++ ch03/string_int_conv/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let guess = "42".parse().expect("Not a number!");
+    let guess: u32 = "42".parse().expect("Not a number!");
     println!("A guess to make a dolphin proud! {guess}");
 }
