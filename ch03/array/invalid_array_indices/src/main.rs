e466d0e6b0ce96b0af99b8427e98136339315e90
Larry Jones <mrwizard82d1@gmail.com>
2025-05-23 19:59:42 -0500

    Invalid array index access

---
+++ ch03/array/invalid_array_indices/src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+    let a = [1, 1, 2, 3, 5];
+
+    println!("Please enter an array index.");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read index line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Entered index was not a number");
+
+    let element = a[index];
+    println!("The value of the element at index {index} is: {element}");
+}
