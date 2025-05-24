2ab5572d2ceb9518e480e12a5e318edd8f893cf3
Larry Jones <mrwizard82d1@gmail.com>
2025-05-23 19:28:42 -0500

    Array of month names

---
+++ ch03/array/months_array/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let months = [
+        "January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December",
+    ];
+    println!("Hard-coded months {:?}", months);
+}
