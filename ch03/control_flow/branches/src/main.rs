f29b773fea60b55b106e161978e757a9c983cbf5
Larry Jones <mrwizard82d1@gmail.com>
2025-05-24 20:09:01 -0500

    If statement (takes if branch)

---
+++ ch03/control_flow/branches/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("Condition, `number < 5`, is true");
+    } else {
+        println!("Condition, `number < 5`, is false");
+    }
+}
