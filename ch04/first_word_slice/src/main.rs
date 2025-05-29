3629358cbd91ea16a52f5b6f53ee799660d2c43b
Larry Jones <mrwizard82d1@gmail.com>
2025-05-29 00:49:32 -0500

    First word of a String using a string slice

---
+++ ch04/first_word_slice/src/main.rs
@@ -0,0 +1,19 @@
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i]
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {
+    let text = String::from("To be or not to be");
+
+    let first_word = first_word(&text);
+
+    println!("The first word of the text, <{text}> is <{first_word}>");
+}
