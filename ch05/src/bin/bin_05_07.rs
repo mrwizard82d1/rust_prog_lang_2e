cb916c376cdd31f6aa01df7bab05d50a49bab674
Larry Jones <mrwizard82d1@gmail.com>
2025-05-30 19:57:26 -0500

    Create User struct instances from another instance

---
+++ ch05/src/bin/bin_05_07.rs
@@ -0,0 +1,21 @@
+use ch05::User;
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+    
+    // Because `username` and `email` are of type `String`, the expressions, 
+    // `username: user1.username` and `email: user1.email` involve a **move**
+    // and not a copy.
+    // println!("user1 = {:?}", user1);
+    println!("user2 = {:?}", user2);
+}
