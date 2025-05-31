07fae0ffbfd75d4d656a13d5db18412f502f8388
Larry Jones <mrwizard82d1@gmail.com>
2025-05-31 02:12:44 -0500

    Refactor to use structs to clarify intent

--- ch05/src/bin/b0508_rectangles.rs
+++ ch05/src/bin/b0508_rectangles.rs
@@ -1,9 +1,17 @@
-fn main() {
-    let rect1 = (30, 50);
-    
-    println!("The area of the rectangle is {} square pixels.", area(rect1));
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(dimension: (u32, u32)) -> u32 {
-    dimension.0 * dimension.1
+fn main() {
+    let rect1 = Rectangle{
+        width: 30, 
+        height: 50,
+    };
+    
+    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
