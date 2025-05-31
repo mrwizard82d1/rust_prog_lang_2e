633189c8309bdf25690170ca661f073d5b8af58e
Larry Jones <mrwizard82d1@gmail.com>
2025-05-30 19:31:46 -0500

    Change the value of a User instance

---
+++ ch05/src/bin/bin_05_03.rs
@@ -0,0 +1,14 @@
+use ch05::User;
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    
+    user1.email = String::from("anotheremail@example.com");
+    
+    println!("user1: {:?}", user1);
+}
