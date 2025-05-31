67364e05df0b0b873c5e49ad3756e548f7243294
Larry Jones <mrwizard82d1@gmail.com>
2025-05-31 01:42:04 -0500

    ERROR: Struct data ownership and lifetime

---
+++ ch05/src/bin/struct_data_ownership.rs
@@ -0,0 +1,15 @@
+struct User {
+    active: bool,
+    username: &str,
+    email: &str,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: "someusername123",
+        email: "<EMAIL>",
+        sign_in_count: 1,
+    };
+}
