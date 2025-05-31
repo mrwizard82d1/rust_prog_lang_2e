f0f1fcb62e88aa5888b9a107cf50ed35214c789a
Larry Jones <mrwizard82d1@gmail.com>
2025-05-31 01:54:55 -0500

    Change to use a tuple to model a rectangle
    This model of a rectangle is simple. The state is simply a tuple
    consisting of two items: width and height. However, using a tuple **does
    not** make the semantics of the tuple members clear. (For example, what
    does an expression like `rectangle.0` mean?)

--- ch05/src/bin/b0508_rectangles.rs
+++ ch05/src/bin/b0508_rectangles.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
     
-    println!("The area of the rectangle is {} square pixels.", area(width1, height1));
+    println!("The area of the rectangle is {} square pixels.", area(rect1));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimension: (u32, u32)) -> u32 {
+    dimension.0 * dimension.1
 }
